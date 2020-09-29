f1e28be1ac793a708bdb465f9a8048845f6e15df
Cris <oriol.cristina.25@gmail.com>
2020-09-29 15:58:59 +0200

    src

--- Practica1/src/IndexFiles.java
+++ Practica1/src/IndexFiles.java
@@ -1,5 +1,3 @@
-package org.apache.lucene.demo;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
